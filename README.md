edf01468b9c278fc73b5efc8ce0a87824877ba61
s1r-J <s1r.aria.azureglo@gmail.com>
2018-11-11 10:06:15 +0900

    Add files v0.0.1

---
+++ README.md
@@ -0,0 +1,29 @@
+node-red-contrib-dir2files
+====
+
+Node-RED node that searches files in a directory and return paths which can be filtered by regex.
+
+## Description
+
+fucntions
+
+- filter filenames by regex
+- search files recursively or not
+- select output array type or string type
+
+
+## Usage
+
+![example-flow](./examples/example-flow.png)
+
+You can import the example flow from examples/example.json to your Node-RED.
+
+## Install
+
+## Licence
+
+[Apache-2.0](http://www.apache.org/licenses/LICENSE-2.0.html)
+
+## Author
+
+[s1r-J](https://github.com/s1r-J)
---
+++ dir2files.js
@@ -0,0 +1,55 @@
+module.exports = function (RED) {
+
+    const FS = require('fs-extra');
+    const PATH = require('path');
+
+    function Dir2FilesNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        node.on('input', function (msg) {
+
+            const dirname = config.dirname || msg.dirname || './';
+            const regex = new RegExp(config.pathRegex || msg.pathRegex || '.*');
+            let isRecursive = config.isRecursive;
+            if (!(msg.isRecursive === null || msg.isRecursive === void 0)) {
+                isRecursive = msg.isRecursive;
+            }
+            let isArray = config.isArray;
+            if (!(msg.isArray === null || msg.isArray === void 0)) {
+                isArray = msg.isArray;
+            }
+
+            let filenames = [];
+            const readTopDirSync = ((dirname) => {
+                let items = FS.readdirSync(dirname);
+                items = items.map((itemname) => {
+                    return PATH.join(dirname, itemname);
+                });
+                items.forEach((itempath) => {
+                    if (FS.statSync(itempath).isFile() && regex.test(itempath)) {
+                        filenames.push(itempath);
+                    } else if (FS.statSync(itempath).isDirectory() && isRecursive) {
+                        readTopDirSync(itempath);
+                    }
+                });
+            });
+            readTopDirSync(dirname);
+
+            if (isArray) {
+                msg.payload = filenames;
+                node.send(msg);
+            } else {
+                const lastIndex = filenames.length - 1;
+                for (let i = 0; i < lastIndex; i++) {
+                    msg.payload = filenames[i];
+                    node.send(msg);
+                }
+
+                msg.payload = (filenames[lastIndex] === void 0 ? '' : filenames[lastIndex]);
+                node.send(msg);
+            }
+        });
+    }
+
+    RED.nodes.registerType('dir2files', Dir2FilesNode);
+}
